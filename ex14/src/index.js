9fb806159c8541dbb5a944619489deaf307788b4
amerix94 <77324494+amerix94@users.noreply.github.com>
2021-09-18 10:53:04 +0200

    Add files via upload

---
+++ ex14/src/index.js
@@ -0,0 +1,13 @@
+function main() {
+    var myNoun = "dog";
+    var myVerb = "ran";
+    var myAdjective = "big";
+    var myAdverb = "quickly";
+    var worldBlanks = myNoun + "," + myVerb + "," + myAdjective + "," + myAdverb;
+    
+
+    return worldBlanks;       
+    
+};
+    console.log(main());
+    module.exports = main;
